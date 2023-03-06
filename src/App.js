ec327593971fe79bcee1c5837b6d1bbf73d86ea5
Shubham-Sharma <viku.vsk@gmail.com>
2023-03-06 22:46:31 +0530

    react_assignment2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {Fragment} from 'react'
+import {First} from './challenge_2.js'
+import {Second} from './challenge_2.js'
+import {Third} from './challenge_2.js'
+import {Btn} from './challenge_2.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <First/>
+      <Second/>
+      <Third/>
+      <Btn/>
+    </Fragment>
+    
+  // <Fragment>
+  //     <h1>I am heading one...from first component</h1>
+  //     <h2>I am heading two...from second component</h2>
+  //     <h3>I am heading three...from third component</h3>
+  //     <button>Click me</button>
+  // </Fragment>
+
   );
 }
 
